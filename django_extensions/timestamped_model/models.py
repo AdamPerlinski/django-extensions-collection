92dd2d7375c6370b723e2857d0abff1277ee5532
Adam Perliński <foxtheowner@gmail.com>
2023-02-17 10:21:01 +0100

    Complete TimeStampedModel implementation

--- django_extensions/timestamped_model/models.py
+++ django_extensions/timestamped_model/models.py
@@ -14,3 +14,35 @@ from django.utils import timezone
 
 class TimeStampedModel(models.Model):
     """
+    An abstract base model that provides self-updating
+    'created' and 'modified' fields.
+    """
+    created = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        help_text="Timestamp when this record was created."
+    )
+    modified = models.DateTimeField(
+        auto_now=True,
+        db_index=True,
+        help_text="Timestamp when this record was last modified."
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ['-created']
+        get_latest_by = 'created'
+
+    def save(self, *args, **kwargs):
+        """Override save to handle update_fields properly."""
+        update_fields = kwargs.get('update_fields')
+        if update_fields is not None:
+            kwargs['update_fields'] = set(update_fields) | {'modified'}
+        super().save(*args, **kwargs)
+
+    @property
+    def was_modified(self):
+        """Check if the model was modified after creation."""
+        if self.created and self.modified:
+            return self.modified > self.created
+        return False
