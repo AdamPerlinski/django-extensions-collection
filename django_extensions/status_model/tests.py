337be9ec29488c7d3374a37dc955f70737f4be10
Adam Perliński <foxtheowner@gmail.com>
2023-03-29 11:15:55 +0200

    Expand status_model test coverage

--- django_extensions/status_model/tests.py
+++ django_extensions/status_model/tests.py
@@ -65,3 +65,105 @@ class TestStatusField:
 
 
 class TestStatusModel:
+    """Test cases for StatusModel."""
+
+    def test_model_is_abstract(self):
+        """Test that StatusModel is abstract."""
+        assert StatusModel._meta.abstract is True
+
+    def test_status_changed_field_exists(self):
+        """Test that status_changed field is defined."""
+        field = ConcreteStatusModel._meta.get_field('status_changed')
+        assert field is not None
+        assert field.null is True
+
+    def test_default_status(self, create_tables):
+        """Test default status is set."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+        assert obj.status == 'pending'
+
+    def test_is_status_properties(self, create_tables):
+        """Test auto-generated is_<status> properties."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+
+        assert obj.is_pending is True
+        assert obj.is_processing is False
+        assert obj.is_completed is False
+        assert obj.is_cancelled is False
+
+    def test_set_status(self, create_tables):
+        """Test set_status method."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+
+        old_status = obj.set_status('processing')
+
+        assert old_status == 'pending'
+        assert obj.status == 'processing'
+        assert obj.is_processing is True
+        assert obj.status_changed is not None
+
+    def test_set_status_updates_timestamp(self, create_tables):
+        """Test set_status updates status_changed."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+
+        obj.set_status('processing')
+        first_change = obj.status_changed
+
+        obj.set_status('completed')
+        second_change = obj.status_changed
+
+        assert second_change >= first_change
+
+    def test_set_status_same_value(self, create_tables):
+        """Test set_status with same value doesn't update timestamp."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+
+        obj.set_status('pending')  # Same as default
+
+        assert obj.status_changed is None
+
+    def test_set_status_no_save(self, create_tables):
+        """Test set_status with save=False."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+
+        obj.set_status('processing', save=False)
+
+        obj.refresh_from_db()
+        assert obj.status == 'pending'  # Not saved
+
+    def test_status_age(self, create_tables):
+        """Test status_age property."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+        obj.set_status('processing')
+
+        age = obj.status_age
+        assert age is not None
+        assert age.total_seconds() >= 0
+
+    def test_status_age_none(self, create_tables):
+        """Test status_age is None when never changed."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+        assert obj.status_age is None
+
+    def test_get_available_statuses(self, create_tables):
+        """Test get_available_statuses method."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+        statuses = obj.get_available_statuses()
+
+        assert statuses == ['pending', 'processing', 'completed', 'cancelled']
+
+    def test_get_status_display_name(self, create_tables):
+        """Test get_status_display_name method."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+        assert obj.get_status_display_name() == 'Pending'
+
+        obj.set_status('processing')
+        assert obj.get_status_display_name() == 'Processing'
+
+    def test_status_persists_after_save(self, create_tables):
+        """Test status persists after save."""
+        obj = ConcreteStatusModel.objects.create(name='test')
+        obj.set_status('completed')
+
+        obj.refresh_from_db()
+        assert obj.status == 'completed'
