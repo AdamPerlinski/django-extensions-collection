d3ac5ebca412dac4a32a3484ad7278a93310d48f
Adam Perliński <foxtheowner@gmail.com>
2023-04-08 17:55:07 +0200

    Complete SluggedModel implementation

--- django_extensions/slugged_model/models.py
+++ django_extensions/slugged_model/models.py
@@ -30,3 +30,65 @@ class SluggedModel(models.Model):
         unique=True,
         db_index=True,
         help_text="URL-friendly identifier."
+    )
+
+    class Meta:
+        abstract = True
+
+    def get_slug_source(self):
+        """
+        Override this method to return the string to slugify.
+        Default returns None which skips auto-generation.
+        """
+        return None
+
+    def generate_slug(self):
+        """Generate a unique slug from the source."""
+        source = self.get_slug_source()
+        if not source:
+            return None
+
+        base_slug = slugify(source)[:240]  # Leave room for suffix
+        if not base_slug:
+            base_slug = 'item'
+
+        slug = base_slug
+        counter = 1
+
+        while self._slug_exists(slug):
+            slug = f'{base_slug}-{counter}'
+            counter += 1
+
+        return slug
+
+    def _slug_exists(self, slug):
+        """Check if a slug already exists (excluding self)."""
+        qs = self.__class__.objects.filter(slug=slug)
+        if self.pk:
+            qs = qs.exclude(pk=self.pk)
+        return qs.exists()
+
+    def save(self, *args, **kwargs):
+        """Auto-generate slug if not set."""
+        if not self.slug:
+            self.slug = self.generate_slug()
+        super().save(*args, **kwargs)
+
+    def regenerate_slug(self, save=True):
+        """Force regeneration of the slug."""
+        self.slug = self.generate_slug()
+        if save:
+            self.save(update_fields=['slug'])
+
+    @classmethod
+    def get_by_slug(cls, slug):
+        """Get an object by its slug."""
+        try:
+            return cls.objects.get(slug=slug)
+        except cls.DoesNotExist:
+            return None
+
+    @property
+    def slug_url(self):
+        """Return the slug formatted for use in URLs."""
+        return self.slug.lower()
