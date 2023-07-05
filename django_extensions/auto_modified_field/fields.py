fdc3fc12d5b43531c75f593d8d83dde29096cf09
Adam Perliński <foxtheowner@gmail.com>
2023-07-05 11:03:37 +0200

    Complete AutoModifiedField implementation

--- django_extensions/auto_modified_field/fields.py
+++ django_extensions/auto_modified_field/fields.py
@@ -12,3 +12,29 @@ Usage:
 from django.db import models
 from django.utils import timezone
 
+
+class AutoModifiedField(models.DateTimeField):
+    """
+    A DateTimeField that automatically updates its value to the current
+    datetime every time the object is saved.
+    """
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('editable', False)
+        kwargs.setdefault('db_index', True)
+        super().__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance, add):
+        """Update the field value on every save."""
+        value = timezone.now()
+        setattr(model_instance, self.attname, value)
+        return value
+
+    def deconstruct(self):
+        """Return deconstructed field for migrations."""
+        name, path, args, kwargs = super().deconstruct()
+        if self.editable is False:
+            del kwargs['editable']
+        if kwargs.get('db_index') is True:
+            del kwargs['db_index']
+        return name, path, args, kwargs
