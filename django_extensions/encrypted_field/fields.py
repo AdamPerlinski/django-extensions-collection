8eb03418089ba61848e6be50d2121440afc1301b
Adam Perliński <foxtheowner@gmail.com>
2023-07-26 15:55:26 +0200

    Complete EncryptedField implementation

--- django_extensions/encrypted_field/fields.py
+++ django_extensions/encrypted_field/fields.py
@@ -38,3 +38,80 @@ class EncryptionMixin:
         key = getattr(settings, 'ENCRYPTION_KEY', None)
         if not key:
             raise ValueError(
+                "ENCRYPTION_KEY must be set in Django settings for encrypted fields."
+            )
+
+        # Derive a proper key from the settings key
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=b'django_extensions_salt',
+            iterations=100000,
+        )
+        derived_key = base64.urlsafe_b64encode(kdf.derive(key.encode()))
+        return Fernet(derived_key)
+
+    def encrypt_value(self, value):
+        """Encrypt a string value."""
+        if value is None:
+            return None
+        fernet = self._get_fernet()
+        return fernet.encrypt(value.encode()).decode()
+
+    def decrypt_value(self, value):
+        """Decrypt an encrypted value."""
+        if value is None:
+            return None
+        fernet = self._get_fernet()
+        return fernet.decrypt(value.encode()).decode()
+
+
+class EncryptedCharField(EncryptionMixin, models.CharField):
+    """
+    A CharField that encrypts data before storing in the database
+    and decrypts when retrieved.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Encrypted data is longer than original
+        max_length = kwargs.get('max_length', 255)
+        kwargs['max_length'] = max(max_length * 3, 500)
+        super().__init__(*args, **kwargs)
+
+    def get_prep_value(self, value):
+        """Encrypt value before saving to database."""
+        value = super().get_prep_value(value)
+        return self.encrypt_value(value)
+
+    def from_db_value(self, value, expression, connection):
+        """Decrypt value when loading from database."""
+        return self.decrypt_value(value)
+
+    def to_python(self, value):
+        """Convert value to Python string."""
+        if value is None:
+            return None
+        # If already decrypted (plain string), return as-is
+        return value
+
+
+class EncryptedTextField(EncryptionMixin, models.TextField):
+    """
+    A TextField that encrypts data before storing in the database
+    and decrypts when retrieved.
+    """
+
+    def get_prep_value(self, value):
+        """Encrypt value before saving to database."""
+        value = super().get_prep_value(value)
+        return self.encrypt_value(value)
+
+    def from_db_value(self, value, expression, connection):
+        """Decrypt value when loading from database."""
+        return self.decrypt_value(value)
+
+    def to_python(self, value):
+        """Convert value to Python string."""
+        if value is None:
+            return None
+        return value
