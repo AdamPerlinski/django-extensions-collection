1f36b7cd07624459c0c80b4b287fa7977061e912
Adam Perliński <foxtheowner@gmail.com>
2023-07-27 09:53:34 +0200

    Expand encrypted_field test coverage

--- django_extensions/encrypted_field/tests.py
+++ django_extensions/encrypted_field/tests.py
@@ -72,3 +72,115 @@ class TestEncryptionMixin:
         mixin = EncryptionMixin()
 
         with patch.object(settings, 'ENCRYPTION_KEY', None):
+            with pytest.raises(ValueError, match="ENCRYPTION_KEY must be set"):
+                mixin.encrypt_value("test")
+
+
+class TestEncryptedCharField:
+    """Test cases for EncryptedCharField."""
+
+    def test_is_char_field(self):
+        """Test EncryptedCharField is a CharField."""
+        field = EncryptedCharField(max_length=100)
+        assert isinstance(field, models.CharField)
+
+    def test_max_length_expanded(self):
+        """Test max_length is expanded for encrypted data."""
+        field = EncryptedCharField(max_length=100)
+        assert field.max_length >= 300
+
+    def test_save_and_retrieve(self, create_tables):
+        """Test saving and retrieving encrypted data."""
+        original = "Secret Information"
+        obj = ConcreteEncryptedModel.objects.create(
+            name='test',
+            secret=original,
+            secret_text='text'
+        )
+
+        obj.refresh_from_db()
+        assert obj.secret == original
+
+    def test_different_values_different_encrypted(self, create_tables):
+        """Test different values produce different encrypted data."""
+        obj1 = ConcreteEncryptedModel.objects.create(
+            name='test1',
+            secret='Secret1',
+            secret_text='text'
+        )
+        obj2 = ConcreteEncryptedModel.objects.create(
+            name='test2',
+            secret='Secret2',
+            secret_text='text'
+        )
+
+        # Get raw values from database
+        from django.db import connection
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT secret FROM encrypted_field_concreteencryptedmodel WHERE id = %s",
+                [obj1.pk]
+            )
+            raw1 = cursor.fetchone()[0]
+            cursor.execute(
+                "SELECT secret FROM encrypted_field_concreteencryptedmodel WHERE id = %s",
+                [obj2.pk]
+            )
+            raw2 = cursor.fetchone()[0]
+
+        assert raw1 != raw2
+
+    def test_null_value(self, create_tables):
+        """Test null value handling."""
+        obj = ConcreteEncryptedModel.objects.create(
+            name='test',
+            secret=None,
+            secret_text=None
+        )
+        obj.refresh_from_db()
+        assert obj.secret is None
+
+
+class TestEncryptedTextField:
+    """Test cases for EncryptedTextField."""
+
+    def test_is_text_field(self):
+        """Test EncryptedTextField is a TextField."""
+        field = EncryptedTextField()
+        assert isinstance(field, models.TextField)
+
+    def test_save_and_retrieve(self, create_tables):
+        """Test saving and retrieving encrypted text data."""
+        original = "This is a longer piece of secret text that needs to be encrypted."
+        obj = ConcreteEncryptedModel.objects.create(
+            name='test',
+            secret='secret',
+            secret_text=original
+        )
+
+        obj.refresh_from_db()
+        assert obj.secret_text == original
+
+    def test_multiline_text(self, create_tables):
+        """Test multiline text encryption."""
+        original = "Line 1\nLine 2\nLine 3"
+        obj = ConcreteEncryptedModel.objects.create(
+            name='test',
+            secret='secret',
+            secret_text=original
+        )
+
+        obj.refresh_from_db()
+        assert obj.secret_text == original
+
+    def test_unicode_text(self, create_tables):
+        """Test unicode text encryption."""
+        original = "Hello 世界 🌍 مرحبا"
+        obj = ConcreteEncryptedModel.objects.create(
+            name='test',
+            secret='secret',
+            secret_text=original
+        )
+
+        obj.refresh_from_db()
+        assert obj.secret_text == original
