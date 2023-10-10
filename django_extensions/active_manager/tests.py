f025a8547b41a11d0aaead3e5ee7ba7c132c0574
Adam Perliński <foxtheowner@gmail.com>
2023-10-10 14:00:19 +0200

    Expand active_manager test coverage

--- django_extensions/active_manager/tests.py
+++ django_extensions/active_manager/tests.py
@@ -52,3 +52,86 @@ class TestActiveManager:
         active = ConcreteActiveModel.objects.create(name='active', is_active=True)
         inactive = ConcreteActiveModel.objects.create(name='inactive', is_active=False)
 
+        qs = ConcreteActiveModel.all_objects.all()
+        assert active in qs
+        assert inactive in qs
+
+    def test_active_method(self, create_tables):
+        """Test active() method."""
+        active = ConcreteActiveModel.objects.create(name='active', is_active=True)
+        inactive = ConcreteActiveModel.objects.create(name='inactive', is_active=False)
+
+        qs = ConcreteActiveModel.all_objects.active()
+        assert active in qs
+        assert inactive not in qs
+
+    def test_inactive_method(self, create_tables):
+        """Test inactive() method."""
+        active = ConcreteActiveModel.objects.create(name='active', is_active=True)
+        inactive = ConcreteActiveModel.objects.create(name='inactive', is_active=False)
+
+        qs = ConcreteActiveModel.objects.inactive()
+        assert active not in qs
+        assert inactive in qs
+
+    def test_activate_queryset(self, create_tables):
+        """Test activate() on queryset."""
+        obj = ConcreteActiveModel.objects.create(name='test', is_active=False)
+
+        ConcreteActiveModel.all_objects.filter(pk=obj.pk).activate()
+        obj.refresh_from_db()
+
+        assert obj.is_active is True
+
+    def test_deactivate_queryset(self, create_tables):
+        """Test deactivate() on queryset."""
+        obj = ConcreteActiveModel.objects.create(name='test', is_active=True)
+
+        ConcreteActiveModel.all_objects.filter(pk=obj.pk).deactivate()
+        obj.refresh_from_db()
+
+        assert obj.is_active is False
+
+    def test_all_objects_method(self, create_tables):
+        """Test all_objects() method returns all."""
+        active = ConcreteActiveModel.objects.create(name='active', is_active=True)
+        inactive = ConcreteActiveModel.objects.create(name='inactive', is_active=False)
+
+        qs = ConcreteActiveModel.objects.all_objects()
+        assert active in qs
+        assert inactive in qs
+
+    def test_count_only_active(self, create_tables):
+        """Test count only counts active objects."""
+        ConcreteActiveModel.objects.create(name='active1', is_active=True)
+        ConcreteActiveModel.objects.create(name='active2', is_active=True)
+        ConcreteActiveModel.objects.create(name='inactive', is_active=False)
+
+        assert ConcreteActiveModel.objects.count() == 2
+        assert ConcreteActiveModel.all_objects.count() == 3
+
+
+class TestActiveQuerySet:
+    """Test cases for ActiveQuerySet."""
+
+    def test_toggle(self, create_tables):
+        """Test toggle() method."""
+        obj1 = ConcreteActiveModel.objects.create(name='obj1', is_active=True)
+        obj2 = ConcreteActiveModel.objects.create(name='obj2', is_active=False)
+
+        ConcreteActiveModel.all_objects.all().toggle()
+
+        obj1.refresh_from_db()
+        obj2.refresh_from_db()
+
+        assert obj1.is_active is False
+        assert obj2.is_active is True
+
+    def test_chaining(self, create_tables):
+        """Test queryset method chaining."""
+        ConcreteActiveModel.objects.create(name='a_active', is_active=True)
+        ConcreteActiveModel.objects.create(name='b_active', is_active=True)
+        ConcreteActiveModel.objects.create(name='a_inactive', is_active=False)
+
+        qs = ConcreteActiveModel.all_objects.active().filter(name__startswith='a')
+        assert qs.count() == 1
