c7439a74e0e13bb9f57f61b1dec9b5d09e87a2fa
Adam Perliński <foxtheowner@gmail.com>
2024-03-02 11:25:44 +0100

    Scaffold shell_plus management command

---
+++ django_extensions/shell_plus/__init__.py
@@ -0,0 +1,6 @@
+"""
+shell_plus - Enhanced Django shell with auto-imports.
+
+This extension provides a management command that launches an enhanced
+Django shell with all models automatically imported.
+"""
