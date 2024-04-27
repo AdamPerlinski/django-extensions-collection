a38dc4920f2b1fca10ea36473ad0c480f0d882b5
Adam Perliński <foxtheowner@gmail.com>
2024-04-27 11:55:30 +0200

    Bump version to 0.6.0

--- django_extensions/__init__.py
+++ django_extensions/__init__.py
@@ -1,3 +1,3 @@
-"""Django Extensions Collection - Model mixins and custom fields."""
+"""Django Extensions Collection - Models, fields, validators, tags, and commands."""
 
-__version__ = "0.3.0"
+__version__ = "0.6.0"
