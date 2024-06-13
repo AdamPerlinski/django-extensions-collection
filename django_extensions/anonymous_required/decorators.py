c73e39261354dbc26801e32cf6b8161d869e10f0
Adam Perliński <foxtheowner@gmail.com>
2024-06-13 13:19:44 +0200

    Complete anonymous_required decorator implementation

--- django_extensions/anonymous_required/decorators.py
+++ django_extensions/anonymous_required/decorators.py
@@ -14,3 +14,33 @@ Usage:
         ...
 """
 
+from functools import wraps
+from django.shortcuts import redirect
+from django.conf import settings
+
+
+def anonymous_required(function=None, redirect_url=None):
+    """
+    Decorator that requires the user to NOT be authenticated.
+    Useful for login and registration pages.
+
+    Args:
+        function: The view function to decorate.
+        redirect_url: URL to redirect authenticated users to.
+                     Defaults to settings.LOGIN_REDIRECT_URL or '/'.
+    """
+    def decorator(view_func):
+        @wraps(view_func)
+        def wrapper(request, *args, **kwargs):
+            if request.user.is_authenticated:
+                url = redirect_url or getattr(settings, 'LOGIN_REDIRECT_URL', '/')
+                return redirect(url)
+
+            return view_func(request, *args, **kwargs)
+
+        return wrapper
+
+    if function:
+        return decorator(function)
+
+    return decorator
