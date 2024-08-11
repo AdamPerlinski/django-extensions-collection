6c3ca8ad6b8af712b57a2d93546eec3f7b6b4c8a
Adam Perliński <foxtheowner@gmail.com>
2024-08-11 14:56:08 +0200

    Update test configuration for middleware and decorators

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os
 import django
 from django.conf import settings
 
@@ -5,9 +6,39 @@ def pytest_configure():
     if not settings.configured:
         settings.configure(
             DEBUG=True,
-            DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory:'}},
-            INSTALLED_APPS=['django.contrib.contenttypes', 'django.contrib.auth', 'django_extensions'],
-            SECRET_KEY='test-secret-key',
+            DATABASES={
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': ':memory:',
+                }
+            },
+            INSTALLED_APPS=[
+                'django.contrib.contenttypes',
+                'django.contrib.auth',
+                'django.contrib.sessions',
+                'django_extensions',
+            ],
+            MIDDLEWARE=[
+                'django.middleware.common.CommonMiddleware',
+                'django.middleware.csrf.CsrfViewMiddleware',
+                'django.contrib.sessions.middleware.SessionMiddleware',
+                'django.contrib.auth.middleware.AuthenticationMiddleware',
+            ],
+            ROOT_URLCONF='test_urls',
+            SECRET_KEY='test-secret-key-for-testing-only',
+            USE_TZ=True,
+            TIME_ZONE='UTC',
             DEFAULT_AUTO_FIELD='django.db.models.BigAutoField',
+            TEMPLATES=[{
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.request',
+                    ],
+                },
+            }],
+            ENCRYPTION_KEY='0123456789abcdef0123456789abcdef',
         )
         django.setup()
