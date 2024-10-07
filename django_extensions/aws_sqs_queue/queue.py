346bc4c8d20dccd2c2121b83a17691cad89bf6ca
Adam Perliński <foxtheowner@gmail.com>
2024-10-07 13:44:19 +0200

    Complete SQS queue client implementation

--- django_extensions/aws_sqs_queue/queue.py
+++ django_extensions/aws_sqs_queue/queue.py
@@ -137,3 +137,263 @@ class SQSQueue:
             self._queue_url = response['QueueUrl']
         return self._queue_url
 
+    def send(self, message, delay_seconds=0, attributes=None, deduplication_id=None, group_id=None):
+        """
+        Send a message to the queue.
+
+        Args:
+            message: Message string or dict (will be JSON serialized)
+            delay_seconds: Delay before message is available (0-900)
+            attributes: Optional message attributes
+            deduplication_id: For FIFO queues
+            group_id: For FIFO queues
+
+        Returns:
+            dict: SQS response with MessageId
+        """
+        if isinstance(message, dict):
+            message = json.dumps(message)
+
+        send_args = {
+            'QueueUrl': self.queue_url,
+            'MessageBody': message,
+            'DelaySeconds': delay_seconds,
+        }
+
+        if attributes:
+            send_args['MessageAttributes'] = self._format_attributes(attributes)
+
+        if deduplication_id:
+            send_args['MessageDeduplicationId'] = deduplication_id
+
+        if group_id:
+            send_args['MessageGroupId'] = group_id
+
+        return self.client.send_message(**send_args)
+
+    def send_batch(self, messages, delay_seconds=0):
+        """
+        Send multiple messages in a batch.
+
+        Args:
+            messages: List of messages (strings or dicts)
+            delay_seconds: Delay for all messages
+
+        Returns:
+            dict: SQS response with Successful and Failed lists
+        """
+        entries = []
+        for i, msg in enumerate(messages):
+            if isinstance(msg, dict):
+                msg = json.dumps(msg)
+            entries.append({
+                'Id': str(i),
+                'MessageBody': msg,
+                'DelaySeconds': delay_seconds,
+            })
+
+        # SQS allows max 10 messages per batch
+        results = {'Successful': [], 'Failed': []}
+        for batch_start in range(0, len(entries), 10):
+            batch = entries[batch_start:batch_start + 10]
+            response = self.client.send_message_batch(
+                QueueUrl=self.queue_url,
+                Entries=batch
+            )
+            results['Successful'].extend(response.get('Successful', []))
+            results['Failed'].extend(response.get('Failed', []))
+
+        return results
+
+    def receive(self, max_messages=1, wait_time=0, visibility_timeout=None, attributes=None):
+        """
+        Receive messages from the queue.
+
+        Args:
+            max_messages: Maximum messages to receive (1-10)
+            wait_time: Long polling wait time in seconds (0-20)
+            visibility_timeout: Visibility timeout in seconds
+            attributes: List of attribute names to return
+
+        Yields:
+            SQSMessage objects
+        """
+        receive_args = {
+            'QueueUrl': self.queue_url,
+            'MaxNumberOfMessages': min(max_messages, 10),
+            'WaitTimeSeconds': wait_time,
+        }
+
+        if visibility_timeout is not None:
+            receive_args['VisibilityTimeout'] = visibility_timeout
+
+        if attributes:
+            receive_args['MessageAttributeNames'] = attributes
+        else:
+            receive_args['MessageAttributeNames'] = ['All']
+
+        response = self.client.receive_message(**receive_args)
+
+        for message in response.get('Messages', []):
+            yield SQSMessage(message, self)
+
+    def delete_message(self, receipt_handle):
+        """Delete a message from the queue."""
+        self.client.delete_message(
+            QueueUrl=self.queue_url,
+            ReceiptHandle=receipt_handle
+        )
+
+    def delete_batch(self, receipt_handles):
+        """Delete multiple messages."""
+        entries = [
+            {'Id': str(i), 'ReceiptHandle': rh}
+            for i, rh in enumerate(receipt_handles)
+        ]
+
+        # Max 10 per batch
+        for batch_start in range(0, len(entries), 10):
+            batch = entries[batch_start:batch_start + 10]
+            self.client.delete_message_batch(
+                QueueUrl=self.queue_url,
+                Entries=batch
+            )
+
+    def change_visibility(self, receipt_handle, timeout):
+        """Change message visibility timeout."""
+        self.client.change_message_visibility(
+            QueueUrl=self.queue_url,
+            ReceiptHandle=receipt_handle,
+            VisibilityTimeout=timeout
+        )
+
+    def purge(self):
+        """Purge all messages from the queue."""
+        self.client.purge_queue(QueueUrl=self.queue_url)
+
+    def get_attributes(self, attributes=None):
+        """Get queue attributes."""
+        if attributes is None:
+            attributes = ['All']
+
+        response = self.client.get_queue_attributes(
+            QueueUrl=self.queue_url,
+            AttributeNames=attributes
+        )
+        return response.get('Attributes', {})
+
+    @property
+    def message_count(self):
+        """Get approximate number of messages in queue."""
+        attrs = self.get_attributes(['ApproximateNumberOfMessages'])
+        return int(attrs.get('ApproximateNumberOfMessages', 0))
+
+    def _format_attributes(self, attributes):
+        """Format attributes for SQS."""
+        formatted = {}
+        for key, value in attributes.items():
+            if isinstance(value, str):
+                formatted[key] = {'DataType': 'String', 'StringValue': value}
+            elif isinstance(value, (int, float)):
+                formatted[key] = {'DataType': 'Number', 'StringValue': str(value)}
+            elif isinstance(value, bytes):
+                formatted[key] = {'DataType': 'Binary', 'BinaryValue': value}
+            else:
+                formatted[key] = {'DataType': 'String', 'StringValue': str(value)}
+        return formatted
+
+
+def send_message(queue_name, message, delay_seconds=0, **kwargs):
+    """
+    Send a message to an SQS queue.
+
+    Args:
+        queue_name: Queue name or URL
+        message: Message string or dict
+        delay_seconds: Delay in seconds
+        **kwargs: Additional send_message arguments
+
+    Returns:
+        dict: SQS response
+    """
+    queue = SQSQueue(queue_name=queue_name)
+    return queue.send(message, delay_seconds=delay_seconds, **kwargs)
+
+
+def receive_messages(queue_name, max_messages=1, wait_time=0, delete=False):
+    """
+    Receive messages from an SQS queue.
+
+    Args:
+        queue_name: Queue name or URL
+        max_messages: Maximum messages to receive
+        wait_time: Long polling wait time
+        delete: Whether to delete messages after receiving
+
+    Returns:
+        list: List of message bodies
+    """
+    queue = SQSQueue(queue_name=queue_name)
+    messages = []
+
+    for msg in queue.receive(max_messages=max_messages, wait_time=wait_time):
+        messages.append(msg.body)
+        if delete:
+            msg.delete()
+
+    return messages
+
+
+def delete_message(queue_name, receipt_handle):
+    """Delete a message from an SQS queue."""
+    queue = SQSQueue(queue_name=queue_name)
+    queue.delete_message(receipt_handle)
+
+
+def create_queue(queue_name, attributes=None, tags=None, fifo=False):
+    """
+    Create an SQS queue.
+
+    Args:
+        queue_name: Queue name (add .fifo suffix for FIFO queues)
+        attributes: Optional queue attributes
+        tags: Optional tags dict
+        fifo: Whether to create a FIFO queue
+
+    Returns:
+        str: Queue URL
+    """
+    client = get_sqs_client()
+
+    create_args = {'QueueName': queue_name}
+
+    if attributes is None:
+        attributes = {}
+
+    if fifo:
+        if not queue_name.endswith('.fifo'):
+            queue_name += '.fifo'
+            create_args['QueueName'] = queue_name
+        attributes['FifoQueue'] = 'true'
+
+    if attributes:
+        create_args['Attributes'] = attributes
+
+    if tags:
+        create_args['Tags'] = tags
+
+    response = client.create_queue(**create_args)
+    return response['QueueUrl']
+
+
+def get_queue_url(queue_name):
+    """Get the URL for a queue by name."""
+    client = get_sqs_client()
+    response = client.get_queue_url(QueueName=queue_name)
+    return response['QueueUrl']
+
+
+def delete_queue(queue_url):
+    """Delete an SQS queue."""
+    client = get_sqs_client()
+    client.delete_queue(QueueUrl=queue_url)
