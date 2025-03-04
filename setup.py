cf982be1d855c840c5bb6af5794fd4aa13c41aad
Adam Perliński <foxtheowner@gmail.com>
2025-03-04 06:58:04 +0100

    Update setup.py with all classifiers

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-extensions-collection',
-    version='0.1.0',
-    description='A collection of Django extensions',
-    packages=find_packages(),
-    install_requires=['Django>=3.2'],
+    name='django-extensions-plus',
+    version='1.0.0',
+    description='A collection of 30+ custom Django extensions',
+    author='Django Extensions',
+    author_email='extensions@example.com',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'Django>=3.2',
+        'cryptography>=3.0',
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=7.0',
+            'pytest-django>=4.5',
+            'pytest-cov>=4.0',
+        ],
+    },
     python_requires='>=3.8',
+    classifiers=[
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
 )
